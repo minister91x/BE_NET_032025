c2b13b57183ab9e9f3ee3893738d4975d6321a1b
quannt <quann91.nt@gmail.com>
2025-04-28 21:47:09 +0700

    buoi13

---
+++ BE_032025.ConsoleApp/BE_032025.NetCoreWeb/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using BE_032025.NetCoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE_032025.NetCoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BE_032025.ConsoleApp/BE_032025.NetCoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BE_032025.NetCoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
